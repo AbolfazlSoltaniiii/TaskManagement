9ca80b366a82e24db78f3f2c58f7fbbe31d1d1c1
Soli <soli@gmail.com>
2025-11-14 19:33:25 +0330

    feat : add extraClassName prop to Button component for customizable styling

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,14 +1,14 @@
-const Button = ({ children, type, onClick }) => {
+const Button = ({ children, type, onClick, extraClassName }) => {
   const className = `border border-stone-400 dark:border-stone-600 rounded-lg cursor-pointer bg-gradient-to-bl
-   to-stone-300 from-stone-100 hover:bg-gradient-to-br hover:from-stone-100 hover:to-stone-300
+   to-stone-400 from-stone-200 hover:bg-gradient-to-br hover:from-stone-200 hover:to-stone-400
     focus:outline-stone-700 dark:bg-gradient-to-br dark:text-stone-200 dark:from-stone-700 dark:to-stone-900
        dark:hover:bg-gradient-to-bl dark:hover:from-stone-700 dark:hover:to-stone-900`;
 
   const style = {
-    extraSmall: `${className} px-3 py-2`,
-    small: `${className} px-7 py-2`,
-    base: `${className} px-9 py-3`,
-    large: `${className} py-1.5 w-full`,
+    extraSmall: `${className} px-3 py-2 ${extraClassName}`,
+    small: `${className} px-7 py-2 ${extraClassName}`,
+    base: `${className} px-9 py-3 ${extraClassName}`,
+    large: `${className} py-1.5 w-full ${extraClassName}`,
   };
 
   return (
